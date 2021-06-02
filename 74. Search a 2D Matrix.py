6dc54161647127d48fbed9b54267d6eadcb1ed00
Eason Tang <tangyisheng2@sina.com>
2021-06-02 12:19:40 +0800

    - Add leetcode #718

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -22,35 +22,44 @@ import math
 
 class Solution:
     def searchMatrix(self, matrix, target: int) -> bool:
-        import numpy
         def binarysearch(data, target):
+            """
+            二分法查找
+            :param data:
+            :param target:
+            :return:
+            """
             lower = 0
             upper = len(data) - 1
             mid = math.floor((lower + upper) * 0.5)
             found = False
-            if target == data[mid]: #如果一上来就找到了匹配
-                found = True
-                return found, lower, upper, mid
             while lower <= upper:
+                """
+                进行二分查找
+                注意用小于等于，当三个指针同时指向一个元素
+                则找到了元素
+                """
                 if target < data[mid]:
                     upper = mid - 1
                     mid = math.floor((lower + upper) * 0.5)
                 elif target > data[mid]:
                     lower = mid + 1
                     mid = math.floor((lower + upper) * 0.5)
-                elif target == data[mid]: #有可能在移位之后存在相等的情况
-                    found = True
-                    return found, lower, upper, mid #在相等的情况下返回
-            return found, lower, upper, mid #当移位之后仍然找不到匹配时返回
+                elif target == data[mid]:  # 有可能在移位之后存在相等的情况
+                    return True, mid  # 在相等的情况下返回
+            return found, mid  # 当移位之后仍然找不到匹配时返回
 
-        found, lower_row, upper_row, mid = binarysearch(numpy.asarray(matrix)[:, 0], target)
+        first_row = []
+        for i in range(len(matrix)):
+            first_row.append(matrix[i][0])  # 添加第一行数据
+        found, mid = binarysearch(first_row, target)  # 对每一行的第一列数据进行比较
         if found:
             return found
         else:
-            found, _, _, _ = binarysearch(numpy.asarray(matrix)[mid, :], target)
+            found, _ = binarysearch(matrix[mid], target)  # 对该行剩下的数据进行比较
             return found
 
 
 test = Solution()
-print(test.searchMatrix(matrix=[[1, 3]], target=3))
-pass
+print(test.searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]]
+                        , target=11))   # testcase
