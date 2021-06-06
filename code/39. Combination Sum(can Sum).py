9e215baab57749da5b2914f01ed0fb29a116ad9f
Eason Tang <tangyisheng2@sina.com>
2021-06-06 22:10:57 +0800

    - Add #39. Combination Sum(can Sum).py

---
+++ code/39. Combination Sum(can Sum).py
@@ -0,0 +1,75 @@
+"""
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+Input: candidates = [2], target = 1
+Output: []
+Example 4:
+Input: candidates = [1], target = 1
+Output: [[1]]
+Example 5:
+Input: candidates = [1], target = 2
+Output: [[1,1]]
+
+Constraints:
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+All elements of candidates are distinct.
+1 <= target <= 500
+"""
+
+
+class Solution:
+    memo = {}
+
+    def combinationSum(self, candidates, target: int):
+        """
+        canSum
+        :param candidates:
+        :param target:
+        :return:
+        """
+        if target in self.memo:
+            return self.memo[target]
+        if target == 0:  # basecase: 0无论有没有candidate都能组成，返回true
+            return True
+        if target < 0:
+            return False
+        for num in candidates:
+            remainder = target - num
+            if self.combinationSum(candidates, remainder):
+                self.memo[target] = True
+                return self.memo[target]
+        self.memo[target] = False
+        return self.memo[target]  # 但尝试了所有解法不成功后返回False
+
+test = Solution()
+ret = test.combinationSum([2, 3], 7)
+print(ret)
+test.memo = {}  # 每一个testcase之间清空memo
+ret = test.combinationSum([5, 3, 4, 7], 7)
+print(ret)
+test.memo = {}
+ret = test.combinationSum([2, 4], 7)
+print(ret)
+test.memo = {}
+ret = test.combinationSum([2, 3, 5], 8)
+print(ret)
+test.memo = {}
+ret = test.combinationSum([7, 14], 300)
+print(ret)
+test.memo = {}
