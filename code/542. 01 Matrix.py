628be57888913d9cc373dc7d84dd9e1c967ceb6c
Eason Tang <tangyisheng2@sina.com>
2021-06-10 18:07:07 +0800

    - add #542. 01 Matrix.py(BFS)

---
+++ code/542. 01 Matrix.py
@@ -0,0 +1,63 @@
+"""
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+
+Example 1:
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]
+Example 2:
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+Output: [[0,0,0],[0,1,0],[1,2,1]]
+
+Constraints:
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+mat[i][j] is either 0 or 1.
+There is at least one 0 in mat.
+"""
+import collections
+from typing import List
+
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        def neighbours(cur_x, cur_y, visited):
+            for ne_x, ne_y in [(cur_x + 1, cur_y), (cur_x - 1, cur_y), (cur_x, cur_y + 1), (cur_x, cur_y - 1)]:
+                if 0 <= ne_x < len(mat) and 0 <= ne_y < len(mat[0]) and \
+                        (ne_x, ne_y) not in visited:
+                    yield ne_x, ne_y
+
+        def bfs(x, y):
+            queue = collections.deque()
+            queue.append((x, y, 0))
+            visited = set()
+            visited.add((x, y))
+
+            while queue:
+                cur_x, cur_y, cnt = queue.popleft()
+                if mat[cur_x][cur_y] == 0:
+                    res[x][y] = cnt
+                    break   # 提前剪枝
+                for ne_x, ne_y in neighbours(cur_x, cur_y, visited):
+                    queue.append((ne_x, ne_y, cnt + 1))
+                    visited.add((ne_x, ne_y))
+
+        import numpy
+        res = []
+        for row in range(len(mat)):
+            res.append([0] * len(mat[0]))  # 创建一个和mat相同维度的数组
+        for x in range(len(mat)):
+            for y in range(len(mat[0])):
+                if mat[x][y] == 0:
+                    res[x][y] = 0  # 如果是0则距离直接为0
+                else:
+                    bfs(x, y)  # 否则进行BFS寻找最近的0
+        return res
+
+
+test = Solution()
+ret = test.updateMatrix([[0, 0, 0], [0, 1, 0], [1, 1, 1]])
+print(ret)
