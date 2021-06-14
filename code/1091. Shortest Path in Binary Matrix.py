0bbccc5de4de6741b14d826000137a1c87f3c060
Eason Tang <tangyisheng2@sina.com>
2021-06-14 22:21:09 +0800

    - Update #1091. Shortest Path in Binary Matrix.py (Optimize Code)

--- code/1091. Shortest Path in Binary Matrix.py
+++ code/1091. Shortest Path in Binary Matrix.py
@@ -9,26 +9,30 @@ class Solution:
         :param grid:
         :return:
         """
+
         def neighbours(node):
             x, y = node
             for nx, ny in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x + 1, y + 1), (x + 1, y - 1),
                            (x - 1, y + 1), (x - 1, y - 1)]:
+                # if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and \
+                #         (nx, ny) not in visited and \
+                #         grid[nx][ny] == 0:
                 if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and \
-                        (nx, ny) not in visited and \
                         grid[nx][ny] == 0:
                     yield nx, ny
 
+        n = len(grid)   # 题目中给定矩阵是方阵
         start = (0, 0)
-        end = (len(grid) - 1, len(grid[0]) - 1)
+        end = (n - 1, n - 1)
 
-        if grid[start[0]][start[1]] == 1:  # 判断开始非法情况
+        if grid[start[0]][start[1]] or grid[end[0]][end[1]] == 1:  # 判断开始非法情况
             return -1
 
         step = 1  # 开始时已经算第一步
-        visited = set()
+        # visited = set()
         queue = collections.deque()
         queue.append((start, step))
-        visited.add(start)
+        # visited.add(start)
 
         while queue:
             # BFS
@@ -37,8 +41,10 @@ class Solution:
                 return step
             for nx, ny in neighbours(cur_node):
                 queue.append(((nx, ny), step + 1))
-                visited.add((nx, ny))
+                # visited.add((nx, ny))
+                grid[nx][ny] = -1
         return -1  # 尝试所有方式后无法到达
+
 
 # class Solution:
 #     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
