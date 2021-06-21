ec76a47ad3ef1b709967c310f94add0c1f05d08a
Eason Tang <tangyisheng2@sina.com>
2021-06-21 15:35:04 +0800

    - Update #125. Valid Palindrome.py(Super Fast)

--- code/125. Valid Palindrome.py
+++ code/125. Valid Palindrome.py
@@ -7,7 +7,7 @@
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(ch.lower() for ch in s if ch.isalnum()) # Ignore Cases, space, and markings
+        s = "".join(ch.lower() for ch in s if ch.isalnum())  # Ignore Cases, space, and markings
         i = 0
         j = len(s) - 1
         while i < j:
@@ -16,3 +16,15 @@ class Solution:
             i += 1
             j -= 1
         return True
+
+# class Solution: # Super Fast!!!
+#     def isPalindrome(self, s: str) -> bool:
+#         if s == '':
+#             return True
+#         s = s.lower()
+#         for i in ',.:;!?@%&$`#()~_[]{}\\/\'"|- ':
+#             s = s.replace(i, '')
+#         if s == s[::-1]:
+#             return True
+#         else:
+#             return False
