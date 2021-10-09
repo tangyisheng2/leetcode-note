c76b6740b49b116e7a09c885e422dc6861fc2588
Eason Tang <tangyisheng2@sina.com>
2021-10-08 18:27:42 -0700

    Add 76. Minimum Window Substring.py
    Signed-off-by: Tang Eason <tangyisheng2@sina.com>

--- code/76. Minimum Window Substring.py
+++ code/76. Minimum Window Substring.py
@@ -35,7 +35,7 @@ class Solution:
 # 链接：https://leetcode-cn.com/problems/minimum-window-substring/solution/tong-su-qie-xiang-xi-de-miao-shu-hua-dong-chuang-k/
 # 来源：力扣（LeetCode）
 # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
+# https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/hua-dong-chuang-kou-by-powcai/
 
 test = Solution()
 test.minWindow(s="ADOBECODEBANC", t="ABC")
