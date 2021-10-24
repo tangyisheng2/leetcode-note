28f2da7af1ee498137757a23e08500e7a28b3ad1
Tang Eason <tangyisheng2@sina.com>
2021-10-24 11:22:00 -0700

    Update 79. Word Search.py
    Signed-off-by: Tang Eason <tangyisheng2@sina.com>

--- code/79. Word Search.py
+++ code/79. Word Search.py
@@ -11,6 +11,8 @@ from typing import List
 "AAB"
 在BFS时候，可能会出现board和word都有多次重复的情况，在这个时候BFS比较难处理这种问题
 """
+
+
 # class Solution:
 #     def exist(self, board: List[List[str]], word: str) -> bool:
 #         """
@@ -58,36 +60,78 @@ from typing import List
 #     # [["C","A","A"],["A","A","A"],["B","C","D"]]
 #     # "AAB"
 
+# class Solution:
+#     """
+#     使用深度优先搜索：
+#     1. 只需要求一个唯一解
+#     2. 解的长度是固定的，因此在深的不大的情况下深度优先搜索和广度优先搜索的复杂度几乎相同
+#     """
+#
+#     def exist(self, board: List[List[str]], word: str) -> bool:
+#
+#         def dfs(cur_x, cur_y, ch_idx):
+#             if board[cur_x][cur_y] != word[ch_idx]:  # 如果当前数字和word不相符
+#                 return False
+#             if ch_idx == len(word) - 1:  # 如果数字相符，且已经达到了末尾
+#                 return True
+#
+#             result = False
+#             visited.add((cur_x, cur_y))
+#             for ne_x, ne_y in [(cur_x + 1, cur_y), (cur_x - 1, cur_y), (cur_x, cur_y + 1), (cur_x, cur_y - 1)]:
+#                 if 0 <= ne_x < len(board) and 0 <= ne_y < len(board[0]) and \
+#                         (ne_x, ne_y) not in visited:  # 判断是否有遍历过
+#                     if dfs(ne_x, ne_y, ch_idx + 1):
+#                         result = True
+#                         break
+#
+#             visited.remove((cur_x, cur_y))  # 在遍历完成之后要删除掉visited节点中的坐标，防止影响下次遍历
+#             return result
+#
+#         m = len(board)
+#         n = len(board[0])
+#         visited = set()
+#         for x in range(m):
+#             for y in range(n):
+#                 if dfs(x, y, 0):
+#                     return True
+#
+#         return False
+
+
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-
-        def dfs(cur_x, cur_y, ch_idx):
-            if board[cur_x][cur_y] != word[ch_idx]:  # 如果当前数字和word不相符
+    def exist(self, board: List[List[str]], word: str):
+        """
+        使用深度优先搜索：
+        1. 只需要求一个唯一解
+        2. 解的长度是固定的，因此在深的不大的情况下深度优先搜索和广度优先搜索的复杂度几乎相同
+        """
+        def dfs(x, y, ch_idx):
+            if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]) or board[x][y] != word[ch_idx]:
+                # 检验非法情况（越界，数字不相等）
                 return False
-            if ch_idx == len(word) - 1:  # 如果数字相符，且已经达到了末尾
-                return True
+            if ch_idx == len(word) - 1:
+                return True  # basecase, 此时默认board[x][y] == word[ch_idx]
 
-            result = False
-            visited.add((cur_x, cur_y))
-            for ne_x, ne_y in [(cur_x + 1, cur_y), (cur_x - 1, cur_y), (cur_x, cur_y + 1), (cur_x, cur_y - 1)]:
-                if 0 <= ne_x < len(board) and 0 <= ne_y < len(board[0]) and \
-                        (ne_x, ne_y) not in visited:    # 判断是否有遍历过
-                    if dfs(ne_x, ne_y, ch_idx + 1):
-                        result = True
-                        break
+            """
+            下面是一个设置visited数组的小撇步，在每一次递归中将下一个要递归的元素用临时变量存起来
+            并且在递归结束后复原
+            （在调用栈出栈的时候会逐步吧所有更改过的元素复原）
+            """
+            tmp = board[x][y]
+            board[x][y] = ''
+            found = dfs(x + 1, y, ch_idx + 1) or \
+                    dfs(x - 1, y, ch_idx + 1) or \
+                    dfs(x, y + 1, ch_idx + 1) or \
+                    dfs(x, y - 1, ch_idx + 1)
+            """
+            DFS搜寻相邻元素，只要有一个元素返回真即可
+            PS. or 有短路性质，只要前一个为真，后面的元素就不再搜索
+            """
+            board[x][y] = tmp
 
-            visited.remove((cur_x, cur_y))  # 在遍历完成之后要删除掉visited节点中的坐标，防止影响下次遍历
-            return result
+            return found
 
-        m = len(board)
-        n = len(board[0])
-        visited = set()
-        for x in range(m):
-            for y in range(n):
-                if dfs(x, y, 0):
-                    return True
-
-        return False
+        return any(dfs(x, y, 0) for x in range(len(board)) for y in range(len(board[0])))
 
 
 test = Solution()
