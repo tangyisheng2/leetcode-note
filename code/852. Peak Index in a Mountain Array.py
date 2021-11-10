02068947b0a5cce5be2c117ee8fde95a9cd229ea
Eason Tang <tangyisheng2@sina.com>
2021-11-09 18:51:13 -0800

    Update 852. Peak Index in a Mountain Array.py
    Signed-off-by: Tang Eason <tangyisheng2@sina.com>

--- code/852. Peak Index in a Mountain Array.py
+++ code/852. Peak Index in a Mountain Array.py
@@ -5,6 +5,7 @@
 # @Author    :Eason Tang
 from typing import List
 
+
 # class Solution:
 #     # Linear Solution
 #     def peakIndexInMountainArray(self, arr: List[int]) -> int:
@@ -14,18 +15,35 @@ from typing import List
 #         return "-1"
 
 
-class Solution:
-    # Binary Search
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        lo = 0
-        hi = len(arr) - 1
-        ans = 0
+# class Solution:
+#     # Binary Search
+#     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+#         lo = 0
+#         hi = len(arr) - 1
+#         ans = 0
+#
+#         while lo <= hi:
+#             mid = (lo + hi) // 2
+#             if arr[mid] > arr[mid + 1]:
+#                 ans = mid
+#                 hi = mid - 1
+#             else:
+#                 lo = mid + 1
+#         return ans
 
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if arr[mid] > arr[mid + 1]:
-                ans = mid
-                hi = mid - 1
+
+class Solution:
+    def peakIndexInMountainArray(self, arr):
+        l, r = 0, len(arr)
+        while l <= r:
+            m = l + (r - l) // 2
+            if arr[m] > arr[m + 1]:
+                r = m - 1
             else:
-                lo = mid + 1
-        return ans
+                l = m + 1
+        return l
+
+
+test = Solution()
+ret = test.peakIndexInMountainArray(arr=[24, 69, 100, 99, 79, 78, 67, 36, 26, 19])
+print(ret)
