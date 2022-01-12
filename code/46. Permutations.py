ebe4a6fe674886b0d2093de998808710a387d61a
tangyisheng2 <tangyisheng2@sina.com>
2022-01-11 23:10:49 -0800

    Update 46. Permutations.py
    Signed-off-by: Tang Eason <tangyisheng2@sina.com>
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- code/46. Permutations.py
+++ code/46. Permutations.py
@@ -8,62 +8,85 @@
 from typing import List
 
 
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         """
+#         Base case:
+#         1. Only 1 element: return [[n]]
+#         2. Only 2 elements: swap the 2 element
+#
+#         >= 3 elements: fix the first element the recuse the rest
+#         """
+#
+#         def dfs(prefix, depth, max_depth, used):
+#             if depth == max_depth:  # basecase
+#                 ans.append(prefix[:])
+#                 # 在 Java 中，参数传递是 值传递，对象类型变量在传参的过程中，复制的是变量的地址。这些地址被添加到 res 变量，但实际上指向的是同一块内存地址，因此我们会看到 6
+#                 # 个空的列表对象。解决的方法很简单，在 res.add(path); 这里做一次拷贝即可。
+#                 # 链接：https://leetcode-cn.com/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/
+#
+#                 return
+#
+#             for i in range(len(nums)):
+#                 if not used[i]:
+#                     # 递归
+#                     used[i] = True
+#                     prefix.append(nums[i])
+#                     dfs(prefix, depth + 1, max_depth, used)
+#                     # 复原
+#                     used[i] = False
+#                     prefix.pop()
+#             return ans
+#
+#         ans = []
+#         used = [False for _ in range(len(nums))]
+#         return dfs([], 0, len(nums), used)
+#
+#
+# # New Version
+# class Solution2:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         def dfs(nums, used, current_state, ans):
+#             if len(current_state) == len(nums):
+#                 ans.append(current_state[:])
+#                 return
+#
+#             for i in range(len(nums)):
+#                 if not used[i]:
+#                     used[i] = True
+#                     current_state.append(nums[i])
+#                     dfs(nums, used, current_state, ans)
+#                     current_state.pop()
+#                     used[i] = False
+#
+#         ans = []
+#         used = [False for _ in range(len(nums))]
+#         dfs(nums, used, [], ans)
+#         return ans
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        """
-        Base case:
-        1. Only 1 element: return [[n]]
-        2. Only 2 elements: swap the 2 element
-
-        >= 3 elements: fix the first element the recuse the rest
-        """
-
-        def dfs(prefix, depth, max_depth, used):
-            if depth == max_depth:  # basecase
-                ans.append(prefix[:])
-                # 在 Java 中，参数传递是 值传递，对象类型变量在传参的过程中，复制的是变量的地址。这些地址被添加到 res 变量，但实际上指向的是同一块内存地址，因此我们会看到 6
-                # 个空的列表对象。解决的方法很简单，在 res.add(path); 这里做一次拷贝即可。
-                # 链接：https://leetcode-cn.com/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/
-
-                return
-
-            for i in range(len(nums)):
-                if not used[i]:
-                    # 递归
-                    used[i] = True
-                    prefix.append(nums[i])
-                    dfs(prefix, depth + 1, max_depth, used)
-                    # 复原
-                    used[i] = False
-                    prefix.pop()
-            return ans
-
         ans = []
-        used = [False for _ in range(len(nums))]
-        return dfs([], 0, len(nums), used)
+        state = []
+        availiable_nums = set(nums)
 
+        def backtracking():
+            if len(state) == len(nums):
+                ans.append(state[:])
 
-# New Version
-class Solution2:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(nums, used, current_state, ans):
-            if len(current_state) == len(nums):
-                ans.append(current_state[:])
-                return
+            print(f"Current State:  {state}")
 
-            for i in range(len(nums)):
-                if not used[i]:
-                    used[i] = True
-                    current_state.append(nums[i])
-                    dfs(nums, used, current_state, ans)
-                    current_state.pop()
-                    used[i] = False
+            for num in availiable_nums:
+                state.append(num)
+                availiable_nums.remove(num)
 
-        ans = []
-        used = [False for _ in range(len(nums))]
-        dfs(nums, used, [], ans)
+                backtracking()
+                state.pop()
+                availiable_nums.add(num)
+                print(f"Backtracking {state}")
+
+        backtracking()
         return ans
-
 
 if __name__ == '__main__':
     test = Solution()
