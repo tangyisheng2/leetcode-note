a047ec0a2541b72ea9520665c3be5fff44795e7e
tangyisheng2 <tangyisheng2@sina.com>
2022-04-04 14:03:04 -0700

    Add 322. Coin Change.py
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- code/322. Coin Change.py
+++ code/322. Coin Change.py
@@ -19,13 +19,45 @@ class Solution:
         for i in range(len(dp)):
             for coin in coins:
                 if i + coin < len(dp) and dp[i] != -1:
-                    if dp[i+coin] == -1:
+                    if dp[i + coin] == -1:
                         dp[i + coin] = dp[i] + 1
                     else:
                         dp[i + coin] = min(dp[i] + 1, dp[i + coin])
         return dp[amount]
 
+    def coinChange2(self, coins: List[int], amount: int) -> int:
+        # def dp(rem) -> int:
+        #     if rem < 0: return -1
+        #     if rem == 0: return 0
+        #     mini = int(1e9)
+        #     for coin in coins:
+        #         res = dp(rem - coin)
+        #         if res >= 0 and res < mini:
+        #             mini = res + 1
+        #     return mini if mini < int(1e9) else -1
+
+        # if amount < 1: return 0
+        # return dp(amount)
+
+        def solve(remainder, cnt):
+            nonlocal ans
+            if remainder < 0:
+                return
+
+            if remainder == 0:
+                ans = min(ans, cnt)
+                return
+
+            for coin in coins:
+                solve(remainder - coin, cnt + 1)
+
+        ans = int(1e9)
+        coins.sort(reverse=True)
+        solve(amount, 0)
+
+        return ans
+
 
 test = Solution()
-ret = test.coinChange(coins=[2], amount=3)
+ret = test.coinChange2(coins=[1, 2, 5], amount=11)
 print(ret)
