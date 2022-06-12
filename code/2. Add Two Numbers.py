bcc5710a1ee48e130ba73a81cdb8eea3fd60365b
tangyisheng2 <tangyisheng2@sina.com>
2022-06-12 11:49:48 -0700

    Update 2. Add Two Numbers.py
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- code/2. Add Two Numbers.py
+++ code/2. Add Two Numbers.py
@@ -1,4 +1,7 @@
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -6,42 +9,42 @@ class ListNode:
 
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        """
-        从题目中的ListNode与实际运算的数字反向
-        我们可以使用堆栈数据结构的先入先出特性来将数字反序并进行相加
-        """
-        l1_stack, l2_stack = [], []  # 新建堆栈
-        l1_res, l2_res = 0, 0  # 新建两个变量存储l1, l2用于计算的值
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Convert the s1, s2 linkedlist to stack (note the linkedlist comes in reverse order)
+        s1 = []
         while l1:
-            l1_stack.append(l1.val)  # l1中元素逐个入栈
-            l1 = l1.next  # l1指针后移
-        while l1_stack:
-            l1_res = l1_stack.pop() + l1_res * 10  # l1栈中元素逐个出栈，并且相加获得l1数字
+            s1.append(l1.val)
+            l1 = l1.next
+
+        s2 = []
         while l2:
-            l2_stack.append(l2.val)
+            s2.append(l2.val)
             l2 = l2.next
-        while l2_stack:
-            l2_res = l2_stack.pop() + l2_res * 10
 
-        res = l1_res + l2_res  # 进行运算
+        ans = []
+        # Initial carry is 0
+        carry = 0
 
-        res = list(str(res))  # 这里将运算完成的结果转为str方便拆分成list
-        res_stack = []
+        for i in range(max(len(s1), len(s2))):
+            # Add the numbers
+            digit = ((s1[i] if i < len(s1) else 0) + (s2[i] if i < len(s2) else 0) + carry)
+            # Append the last digit
+            ans.append(digit % 10)
+            # calculate the carry for next add
+            carry = digit // 10
+        # If still have a carry at the end
+        if carry:
+            ans.append(carry)
 
-        for i in range(len(res)):
-            res_stack.append(int(res[i]))  # 转换结果入栈，准备倒序处理
+        # Covert ans array to linkedlist
+        head = ListNode()
+        cur = head
 
-        res_listnode = ListNode()
-        res_listnode_ptr = res_listnode
-        while res_stack:
-            num = res_stack.pop()  # 数组出栈
-            res_listnode_ptr.val = num  # 构造链表
-            if res_stack:  # 检测插入完节点之后还有没有多余的元素，如果没有就不需要在新建next域了
-                res_listnode_ptr.next = ListNode()  # 链接链表的下一个节点
-                res_listnode_ptr = res_listnode_ptr.next
+        for num in ans:
+            cur.next = ListNode(num)
+            cur = cur.next
 
-        return res_listnode
+        return head.next
 
 
 # L1
