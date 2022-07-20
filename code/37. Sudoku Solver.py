0d6bd2b9b812d2d14cbb379c57fbf35670b2e3a7
tangyisheng2 <tangyisheng2@sina.com>
2022-07-19 21:35:43 -0700

    Update 37. Sudoku Solver.py
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- code/37. Sudoku Solver.py
+++ code/37. Sudoku Solver.py
@@ -58,6 +58,46 @@ class Solution:
         print(board)
 
 
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        def dfs(pos: int):
+            """
+            按行遍历数独
+            """
+            nonlocal valid
+            if pos == len(spaces):  # 如果速度遍历完成（遍历到最后一行的下一行），则数独有解
+                valid = True
+                return
+
+            i, j = spaces[pos]  # 每一行的第一个空格开始便利
+            for digit in range(9):
+                if line[i][digit] == column[j][digit] == block[i // 3][j // 3][
+                    digit] == False:  # 如果在每一行，每一列，每一个九宫格中均没有占用
+                    line[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True  # 更新占用情况
+                    board[i][j] = str(digit + 1)  # 更新数独元素
+                    dfs(pos + 1)  # 更新spaces数组中的下一个元素
+                    line[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = False  # backtracking回溯
+                if valid:  # 如果数独合法，early stop
+                    return
+
+        line = [[False] * 9 for _ in range(9)]  # 一维：每一行的行坐标；二维：每一行中数字i（下标为i - 1）是否出现
+        column = [[False] * 9 for _ in range(9)]  # 一维：每一行的行坐标；二维：每一列中数字i（下标为i - 1）是否出现
+        block = [[[False] * 9 for _a in range(3)] for _b in
+                 range(3)]  # 一维：每一个九宫格的坐标（从左到右从上到下）；二维：每一九宫格中数字i（下标为i - 1）是否出现
+        valid = False  # 数独已经有解
+        spaces = list()  # 空格位置
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    spaces.append((i, j))  # 遇到空格将其添加进spaces数组中，用于遍历
+                else:
+                    digit = int(board[i][j]) - 1
+                    line[i][digit] = column[j][digit] = block[i // 3][j // 3][digit] = True  # 否则将数字添加到used数组中
+
+        dfs(0)
+
+
 test = Solution()
 test.solveSudoku(board=[["5", "3", ".", ".", "7", ".", ".", ".", "."],
                         ["6", ".", ".", "1", "9", "5", ".", ".", "."],
